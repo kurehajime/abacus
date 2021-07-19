2dd65f5012daf1651d30aad00a7d8da9d116f158
kurehajime <xiidec@gmail.com>
2021-07-19 22:13:57 +0900

    add event

--- src/zhu.js
+++ src/zhu.js
@@ -4,16 +4,34 @@ export class InputZhu extends HTMLElement {
         this.attachShadow({
             mode: 'open'
         });
+
         this.shadowRoot.innerHTML = `
 <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 100 100">
-    <polygon points="0 50, 50 20, 100 50" stroke-width="1" stroke="#8d6449" fill="#8d6449" />
-    <polygon points="0 50, 50 80, 100 50" stroke-width="1" stroke="#96514d" fill="#96514d" />
+    <polygon id="up" points="0 50, 50 20, 100 50" stroke-width="1" stroke="#8d6449" fill="#8d6449" />
+    <polygon id="down" points="0 50, 50 80, 100 50" stroke-width="1" stroke="#96514d" fill="#96514d" />
 </svg>
         `
     }
 
     // 読み込み時
     connectedCallback() {
+
+        this.up = this.shadowRoot.getElementById('up');
+        this.down = this.shadowRoot.getElementById('down');
+
+        this.up.addEventListener('mousemove', (e) => {
+            if (e.buttons === 1) { console.log("up") }
+        });
+        this.up.addEventListener('mousedown', (e) => {
+            if (e.buttons === 1) { console.log("up") }
+        });
+
+        this.down.addEventListener('mousemove', (e) => {
+            if (e.buttons === 1) { console.log("down") }
+        });
+        this.down.addEventListener('mousedown', (e) => {
+            if (e.buttons === 1) { console.log("down") }
+        });
 
     }
 
@@ -24,8 +42,8 @@ export class InputZhu extends HTMLElement {
     }
 
     // 監視イベント
-    attributeChangedCallback() {
-        switch (null) {
+    attributeChangedCallback(name, oldValue, newValue) {
+        switch (name) {
             default:
                 break;
         }
